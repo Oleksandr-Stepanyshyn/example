f7bb976b1a86506cf1c92d80fc9e024525a6a555
Oleksandr-Stepanyshyn <88288677+Oleksandr-Stepanyshyn@users.noreply.github.com>
2022-04-04 19:06:57 +0300

    add files

---
+++ index.js
@@ -0,0 +1,3 @@
+// import './js/module-11/task-02.js';
+// import './css/module-11/task-02.css';
+// import './js/test-for-work/test-01';
---
+++ js/test-for-work/test-01.js
@@ -0,0 +1,12 @@
+console.log('hello');
+
+const pass = '%63%70%61%73%62%69%65%6e%64%75%72%70%61%73%73%77%6f%72%64';
+
+console.log(decodeURI(pass));
+
+
+console.log(decodeURI(pass));
+
+
+
+
